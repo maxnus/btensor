7ec38998f72701980e077968cc0b0c53e52a7b22
Max Nusspickel <max.nusspickel@gmail.com>
2023-05-28 10:30:44 +0100

    Updates test_util.py to pytest fixtures

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,11 +1,12 @@
-import unittest
 import itertools
+import pytest
 import numpy as np
+
 from btensor import util
-from testing import TestCase, powerset
+from helper import TestCase, powerset
 
 
-class UtilTests(TestCase):
+class TestPermutationMatrix(TestCase):
 
     def test_combine_permutation_matrices(self):
         n, m, k = 10, 8, 6
@@ -28,95 +29,87 @@ class UtilTests(TestCase):
         self.assert_allclose(util.MatrixProduct((r1, c1)).evaluate(), np.dot(r1.to_array(), c1.to_array()))
         self.assert_allclose(util.MatrixProduct((r2, c2)).evaluate(), np.dot(r2.to_array(), c2.to_array()))
 
+    @staticmethod
+    def get_permutation_matrix_input():
+        size = 5
+        return [
+            [], [0], [4],
+            np.arange(size), np.arange(5), np.random.permutation(range(size))[:5],
+            slice(None), slice(0, 5), slice(5, size), slice(None, None, 2), slice(None, None, -2), slice(3, 8)
+        ]
 
-def generate_test_permutation_matrix(cls, column=True):
-    n = 10
+    @pytest.fixture(params=get_permutation_matrix_input(), ids=lambda x: str(x))
+    def permutation_matrix_input(self, request):
+        return request.param
 
-    def generate_test(perm):
+    @pytest.fixture(params=['row', 'column'])
+    def permtuation_matrix_type(self, request):
+        return request.param
+
+    def test_permutation_matrix(self, permutation_matrix_input, permtuation_matrix_type):
+        perm = permutation_matrix_input
+        n = 5
         m = len(np.arange(n)[perm])
-
-        def test(self):
-            nonlocal perm
-            if column:
-                p = util.ColumnPermutationMatrix(permutation=perm, size=n)
-                pt = p.T
-            else:
-                pt = util.RowPermutationMatrix(permutation=perm, size=n)
-                p = pt.T
-            # Test transpose
-            self.assert_allclose(p.to_array().T, pt.to_array())
-            # Test p.T x p
-            self.assert_allclose(pt.to_array().dot(p.to_array()), np.identity(m))
-            # Test p x p.T
-            ppt = p.to_array().dot(pt.to_array())
-            self.assert_allclose(ppt - np.diag(np.diag(ppt)), 0)
-            nonzero = np.diag(ppt).nonzero()[0]
-            if isinstance(perm, slice):
-                perm = np.arange(n)[perm]
-            expected = set(np.asarray(perm).tolist())
-            self.assert_allclose(set(nonzero), expected)
-        return test
-
-    # Test permutations:
-    perms = [
-        [], [0], [4],
-        np.arange(n), np.arange(5), np.random.permutation(range(n))[:5],
-        slice(None), slice(0, 5), slice(5, n), slice(None, None, 2), slice(None, None, -2), slice(3, 8),
-    ]
-    for m, perm in enumerate(perms):
-        funcname = 'test_%s_permutation_matrix_%s_%d' % ('column' if column else 'row', type(perm).__name__, m)
-        print("Adding test '%s'" % funcname)
-        assert not hasattr(cls, funcname)
-        setattr(cls, funcname, generate_test(perm))
+        if permtuation_matrix_type == 'column':
+            p = util.ColumnPermutationMatrix(permutation=perm, size=n)
+            pt = p.T
+        elif permtuation_matrix_type == 'row':
+            pt = util.RowPermutationMatrix(permutation=perm, size=n)
+            p = pt.T
+        # Test transpose
+        self.assert_allclose(p.to_array().T, pt.to_array())
+        # Test p.T x p
+        self.assert_allclose(pt.to_array().dot(p.to_array()), np.identity(m))
+        # Test p x p.T
+        ppt = p.to_array().dot(pt.to_array())
+        self.assert_allclose(ppt - np.diag(np.diag(ppt)), 0)
+        nonzero = np.diag(ppt).nonzero()[0]
+        if isinstance(perm, slice):
+            perm = np.arange(n)[perm]
+        expected = set(np.asarray(perm).tolist())
+        self.assert_allclose(set(nonzero), expected)
 
 
-def generate_test_chained_dot(cls, simplify=True, atol=1e-10):
-    n = 10
+class TestMatrixProduct(TestCase):
 
-    def generate_test(*args):
-        def test(self):
-            args_ref = [x for x in args if x is not None]
-            args_ref = [(x.to_array() if hasattr(x, 'to_array') else x) for x in args_ref]
-            if len(args_ref) == 0:
-                ref = None
-            elif len(args_ref) == 1:
-                ref = args_ref[0]
-            else:
-                ref = np.linalg.multi_dot(args_ref)
-            mpl = util.MatrixProduct(args)
-            self.assert_allclose(mpl.evaluate(simplify=simplify), ref, atol=atol, rtol=0)
-        return test
+    @pytest.fixture(params=['', 'simplify'])
+    def matrix_product_simplify(self, request):
+        return request.param
 
-    a = util.GeneralMatrix(np.random.rand(n, n))
-    matrices = {'x': None,
-                'i': util.IdentityMatrix(n),
-                'a': a,
-                'b': a.inverse,
-                'c': util.ColumnPermutationMatrix(permutation=np.random.permutation(n), size=n),
-                'r': util.RowPermutationMatrix(permutation=np.random.permutation(n), size=n),
-                }
-    matrices = [(k, v) for (k, v) in matrices.items()]
+    @staticmethod
+    def get_matrices():
+        n = 10
+        a = util.GeneralMatrix(np.random.rand(n, n))
+        matrices = {'x': None,
+                    'i': util.IdentityMatrix(n),
+                    'a': a,
+                    'b': a.inverse,
+                    'c': util.ColumnPermutationMatrix(permutation=np.random.permutation(n), size=n),
+                    'r': util.RowPermutationMatrix(permutation=np.random.permutation(n), size=n),
+                    }
+        matrices = [(k, v) for (k, v) in matrices.items()]
 
-    for i, args in enumerate(powerset(matrices, include_empty=False)):
-        if len(args) == 1 and ('x', None) in args:
-            continue
-        for perm in itertools.permutations(args):
-            mats = [p[1] for p in perm]
-            name = ''.join([p[0] for p in perm])
-            funcname = f'test_matrix_product_{"simplify" if simplify else ""}_{name}'
-            print(f"Adding test {funcname}")
-            assert not hasattr(cls, funcname)
-            setattr(cls, funcname, generate_test(*mats))
+        for i, args in enumerate(powerset(matrices, include_empty=False)):
+            if len(args) == 1 and ('x', None) in args:
+                continue
+            for perm in itertools.permutations(args):
+                name = ''.join([p[0] for p in perm])
+                mats = [p[1] for p in perm]
+                yield name, mats
 
+    @pytest.fixture(params=get_matrices(), ids=lambda x: x[0])
+    def matrices(self, request):
+        return request.param
 
-generate_test_permutation_matrix(UtilTests, column=True)
-
-generate_test_permutation_matrix(UtilTests, column=False)
-
-generate_test_chained_dot(UtilTests, simplify=False)
-
-generate_test_chained_dot(UtilTests, simplify=True)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_chained_dot(self, matrices, matrix_product_simplify, atol=1e-10):
+        matrices = matrices[1]
+        args_ref = [x for x in matrices if x is not None]
+        args_ref = [(x.to_array() if hasattr(x, 'to_array') else x) for x in args_ref]
+        if len(args_ref) == 0:
+            ref = None
+        elif len(args_ref) == 1:
+            ref = args_ref[0]
+        else:
+            ref = np.linalg.multi_dot(args_ref)
+        mpl = util.MatrixProduct(matrices)
+        self.assert_allclose(mpl.evaluate(simplify=bool(matrix_product_simplify)), ref, atol=atol, rtol=0)
