9f30cdfc15f205398436fd9bbbbbde184d963c8f
Max Nusspickel <max.nusspickel@gmail.com>
2023-09-24 17:32:02 +0100

    Adds missing exceptions file

---
+++ src/btensor/exceptions.py
@@ -0,0 +1,31 @@
+#     Copyright 2023 Max Nusspickel
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+
+class BTensorError(Exception):
+    pass
+
+
+class BasisError(BTensorError):
+    pass
+
+
+class VarianceError(BTensorError):
+    pass
+
+
+class BasisDependentOperationError(BasisError):
+
+    def __init__(self, msg: str = "operation is basis dependent and therefore not allowed", *args) -> None:
+        super().__init__(msg, *args)
